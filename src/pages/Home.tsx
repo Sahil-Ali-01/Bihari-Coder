ba7c313cf150be1990d8e6a184a2ba4a69ffe6a9
Sahil-Ali-01 <sahilcoder189@gmail.com>
2025-05-16 00:45:31 +0530

    n

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -51,15 +51,6 @@ const Home = () => {
       <main className="flex-grow">
         <HeroMain />
 
-        <div className="text-center my-8">
-          <button
-            onClick={handleButtonClick}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Click Me
-          </button>
-        </div>
-
         {/* Projects Section */}
         <section
           id="projects-section"
